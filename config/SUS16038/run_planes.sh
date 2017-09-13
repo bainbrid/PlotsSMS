fc4839b3e8a73ac3a54641226e80e7705556e142
bainbrid <robert.bainbridge@cern.ch>
2017-09-13 14:16:45 +0100

    triv

--- config/SUS16038/run_planes.sh
+++ config/SUS16038/run_planes.sh
@@ -1,3 +1,4 @@
+mkdir plots/
 cd ../..
 python python/makeSMSplots.py config/SUS16038/T1qqqq_SUS16038.cfg T1qqqq
 python python/makeSMSplots.py config/SUS16038/T1bbbb_SUS16038.cfg T1bbbb
@@ -7,4 +8,5 @@ python python/makeSMSplots.py config/SUS16038/T2bb_SUS16038.cfg T2bb
 python python/makeSMSplots.py config/SUS16038/T2tt_SUS16038.cfg T2tt
 python python/makeSMSplots.py config/SUS16038/T2cc_SUS16038.cfg T2cc
 python python/makeSummarySMSplots.py
+mv *.pdf config/SUS16038/plots/
 cd -
