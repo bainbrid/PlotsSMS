de0ad465dbdcc2269c06e90c7dd7813a9429d248
bainbrid <robert.bainbridge@cern.ch>
2017-09-13 10:46:26 +0100

    produce all individual and summary limit plots

---
+++ config/SUS16038/run_planes.sh
@@ -0,0 +1,10 @@
+cd ../..
+python python/makeSMSplots.py config/SUS16038/T1qqqq_SUS16038.cfg T1qqqq
+python python/makeSMSplots.py config/SUS16038/T1bbbb_SUS16038.cfg T1bbbb
+python python/makeSMSplots.py config/SUS16038/T1tttt_SUS16038.cfg T1tttt
+python python/makeSMSplots.py config/SUS16038/T2qq_SUS16038.cfg T2qq
+python python/makeSMSplots.py config/SUS16038/T2bb_SUS16038.cfg T2bb
+python python/makeSMSplots.py config/SUS16038/T2tt_SUS16038.cfg T2tt
+python python/makeSMSplots.py config/SUS16038/T2cc_SUS16038.cfg T2cc
+python python/makeSummarySMSplots.py
+cd -
