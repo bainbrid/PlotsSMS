c53f986535ee5e7f51f164de94cbf8df560386d0
bainbrid <robert.bainbridge@cern.ch>
2017-10-17 17:34:19 +0100

    tweaked

--- config/SUS16038/get_planes.sh
+++ config/SUS16038/get_planes.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
+INPUT="/Users/bainbrid/Repositories/PlotsSMS/config/SUS16038/temp/"
+
 # Vanilla models
-cp /home/hep/klo2/public_html/RA1/BTagFormula/FullFit/Log/20170713/Unblinding36fb2017_v14_T1bbbb/gridOut_ul.root T1bbbb_expected.root
-cp /home/hep/klo2/public_html/RA1/BTagFormula/FullFit/Log/20170713/Unblinding36fb2017_v14_T1bbbb/gridOutObserved_ul.root T1bbbb_observed.root
-cp /home/hep/klo2/public_html/RA1/BTagFormula/FullFit/Log/20170713/Unblinding36fb2017_v14_T2bb/gridOut_ul.root T2bb_expected.root
-cp /home/hep/klo2/public_html/RA1/BTagFormula/FullFit/Log/20170713/Unblinding36fb2017_v14_T2bb/gridOutObserved_ul.root T2bb_observed.root
-cp /vols/build/cms/sdb15/public/Analysis/RA1/201708_Aug/10_SMS_limitPlane/T1tttt/gridOut_ul.root T1tttt_expected.root
-cp /vols/build/cms/sdb15/public/Analysis/RA1/201708_Aug/10_SMS_limitPlane/T1tttt/gridOutObserved_ul.root T1tttt_observed.root
-cp /vols/build/cms/sdb15/public/Analysis/RA1/201708_Aug/10_SMS_limitPlane/T2tt/gridOut_ul.root T2tt_expected.root
-cp /vols/build/cms/sdb15/public/Analysis/RA1/201708_Aug/10_SMS_limitPlane/T2tt/gridOutObserved_ul.root T2tt_observed.root
-cp /vols/build/cms/sdb15/public/Analysis/RA1/201708_Aug/10_SMS_limitPlane/T2cc/gridOut_ul.root T2cc_expected.root
-cp /vols/build/cms/sdb15/public/Analysis/RA1/201708_Aug/10_SMS_limitPlane/T2cc/gridOutObserved_ul.root T2cc_observed.root
-cp /vols/build/cms/sdb15/public/Analysis/RA1/201708_Aug/10_SMS_limitPlane/T1qqqq/gridOut_ul.root T1qqqq_expected.root
-cp /vols/build/cms/sdb15/public/Analysis/RA1/201708_Aug/10_SMS_limitPlane/T1qqqq/gridOutObserved_ul.root T1qqqq_observed.root
-cp /vols/build/cms/sdb15/public/Analysis/RA1/201708_Aug/10_SMS_limitPlane/T2qq/gridOut_ul.root T2qq_expected.root
-cp /vols/build/cms/sdb15/public/Analysis/RA1/201708_Aug/10_SMS_limitPlane/T2qq/gridOutObserved_ul.root T2qq_observed.root
+cp ${INPUT}T1bbbb/gridOut_ul.root T1bbbb_expected.root
+cp ${INPUT}T1bbbb/gridOutObserved_ul.root T1bbbb_observed.root
+cp ${INPUT}T2bb/gridOut_ul.root T2bb_expected.root
+cp ${INPUT}T2bb/gridOutObserved_ul.root T2bb_observed.root
+cp ${INPUT}T1tttt/gridOut_ul.root T1tttt_expected.root
+cp ${INPUT}T1tttt/gridOutObserved_ul.root T1tttt_observed.root
+cp ${INPUT}T2tt/gridOut_ul.root T2tt_expected.root
+cp ${INPUT}T2tt/gridOutObserved_ul.root T2tt_observed.root
+cp ${INPUT}T2cc/gridOut_ul.root T2cc_expected.root
+cp ${INPUT}T2cc/gridOutObserved_ul.root T2cc_observed.root
+cp ${INPUT}T1qqqq/gridOut_ul.root T1qqqq_expected.root
+cp ${INPUT}T1qqqq/gridOutObserved_ul.root T1qqqq_observed.root
+cp ${INPUT}T2qq/gridOut_ul.root T2qq_expected.root
+cp ${INPUT}T2qq/gridOutObserved_ul.root T2qq_observed.root
 
 # LL models
-cp /vols/build/cms/bainbrid/output/v1.6.x_RB_170910_T1qqqqLL/gridOutObserved_ul.root T1qqqqLL_observed.root
-cp /vols/build/cms/bainbrid/output/v1.6.x_RB_170910_T1qqqqLL/gridOut_ul.root T1qqqqLL_expected.root
+cp ${INPUT}T1qqqqLL/gridOutObserved_ul.root T1qqqqLL_observed.root
+cp ${INPUT}T1qqqqLL/gridOut_ul.root T1qqqqLL_expected.root
--- config/SUS16038/scp_planes.sh
+++ config/SUS16038/scp_planes.sh
@@ -1,30 +1,34 @@
 #!/bin/bash
 
-DIR="rjb3@lx01.hep.ph.ic.ac.uk:/home/hep/klo2/public_html/RA1/BTagFormula/FullFit/Log/20170713/Unblinding36fb2017_v14_" 
-scp ./${DIR}${MODEL}/gridOut_ul.root ${MODEL}_expected.root
-scp ./${DIR}${MODEL}/gridOutObserved_ul.root ${MODEL}_observed.root
-MODEL="T2bb"
-scp ./${DIR}${MODEL}/gridOut_ul.root ${MODEL}_expected.root
-scp ./${DIR}${MODEL}/gridOutObserved_ul.root ${MODEL}_observed.root
+OUTPUT="temp/"
+HOST="rjb3@lx01.hep.ph.ic.ac.uk:"
 
-DIR="rjb3@lx01.hep.ph.ic.ac.uk:/vols/build/cms/sdb15/public/Analysis/RA1/201708_Aug/10_SMS_limitPlane/" 
-MODEL="T1tttt"
-scp ./${DIR}/${MODEL}/gridOut_ul.root ${MODEL}_expected.root
-scp ./${DIR}/${MODEL}/gridOutObserved_ul.root ${MODEL}_observed.root
-MODEL="T2tt"
-scp ./${DIR}/${MODEL}/gridOut_ul.root ${MODEL}_expected.root
-scp ./${DIR}/${MODEL}/gridOutObserved_ul.root ${MODEL}_observed.root
-MODEL="T2cc"
-scp ./${DIR}/${MODEL}/gridOut_ul.root ${MODEL}_expected.root
-scp ./${DIR}/${MODEL}/gridOutObserved_ul.root ${MODEL}_observed.root
+DIR="/vols/build/cms/sdb15/public/Analysis/RA1/201710_Oct/11_SMSUpdate_36invfbGL_v1/planes/" 
 MODEL="T1qqqq"
-scp ./${DIR}/${MODEL}/gridOut_ul.root ${MODEL}_expected.root
-scp ./${DIR}/${MODEL}/gridOutObserved_ul.root ${MODEL}_observed.root
+#scp ${HOST}${DIR}${MODEL}/gridOut_ul.root ${OUTPUT}/${MODEL}_expected.root
+scp ${HOST}${DIR}${MODEL}/gridOutObserved_ul.root ${OUTPUT}/${MODEL}_observed.root
+MODEL="T1bbbb"
+scp ${HOST}${DIR}${MODEL}/gridOut_ul.root ${OUTPUT}/${MODEL}_expected.root
+scp ${HOST}${DIR}${MODEL}/gridOutObserved_ul.root ${OUTPUT}/${MODEL}_observed.root
+MODEL="T1tttt"
+scp ${HOST}${DIR}${MODEL}/gridOut_ul.root ${OUTPUT}/${MODEL}_expected.root
+scp ${HOST}${DIR}${MODEL}/gridOutObserved_ul.root ${OUTPUT}/${MODEL}_observed.root
 MODEL="T2qq"
-scp ./${DIR}/${MODEL}/gridOut_ul.root ${MODEL}_expected.root
-scp ./${DIR}/${MODEL}/gridOutObserved_ul.root ${MODEL}_observed.root
+scp ${HOST}${DIR}${MODEL}/gridOut_ul.root ${OUTPUT}/${MODEL}_expected.root
+scp ${HOST}${DIR}${MODEL}/gridOutObserved_ul.root ${OUTPUT}/${MODEL}_observed.root
+MODEL="T2bb"
+scp ${HOST}${DIR}${MODEL}/gridOut_ul.root ${OUTPUT}/${MODEL}_expected.root
+scp ${HOST}${DIR}${MODEL}/gridOutObserved_ul.root ${OUTPUT}/${MODEL}_observed.root
+MODEL="T2tt"
+scp ${HOST}${DIR}${MODEL}/gridOut_ul.root ${OUTPUT}/${MODEL}_expected.root
+scp ${HOST}${DIR}${MODEL}/gridOutObserved_ul.root ${OUTPUT}/${MODEL}_observed.root
+MODEL="T2cc"
+scp ${HOST}${DIR}${MODEL}/gridOut_ul.root ${OUTPUT}/${MODEL}_expected.root
+scp ${HOST}${DIR}${MODEL}/gridOutObserved_ul.root ${OUTPUT}/${MODEL}_observed.root
 
-BASE="rjb3@lx01.hep.ph.ic.ac.uk:/vols/build/cms/bainbrid/output/v1.6.x_RB_170910_"
+#/vols/build/cms/cl4010/RA1/AlphaStats/output/LL_171016_ctau*
+
+DIR="/vols/build/cms/bainbrid/output/v1.6.x_RB_170910_"
 MODEL="T1qqqqLL"
-scp ./${DIR}${MODEL}/gridOut_ul.root ${MODEL}_expected.root
-scp ./${DIR}${MODEL}/gridOutObserved_ul.root ${MODEL}_observed.root
+scp ${HOST}${DIR}${MODEL}/gridOut_ul.root ${OUTPUT}/${MODEL}_expected.root
+scp ${HOST}${DIR}${MODEL}/gridOutObserved_ul.root ${OUTPUT}/${MODEL}_observed.root
